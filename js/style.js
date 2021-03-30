6e79e52f8bc61c8bb55d017b1f521651ef3856c4
Hinako-Tanaka <youngbird1228@gmail.com>
2021-03-30 16:10:45 +0900

    set

--- js/style.js
+++ js/style.js
@@ -30,4 +30,18 @@ $(function(){
   });
 });
 
+// $(document).ready(function () {
+//   $('.side-bar-btn').click(function (event) {
+//       $(event).find('i').toggleClass('far fa-eye far fa-eye-slash');
+//   });
+// });
+// function fontawesomeChange( element, changeClassName ) {
+//   element.classList.toggle(changeClassName);
+// }
 
+$(function(){
+	$('.toggle_title').click(function(){
+		$(this).toggleClass('selected');
+		$(this).next().slideToggle();
+	});
+});
