b3f3e119485b61425d2af1d0fc180f3dcaef42fc
hinakot1228 <74905456+hinakot1228@users.noreply.github.com>
2021-04-09 12:18:38 +0900

    Revert "accordion menu modify"

--- js/style.js
+++ js/style.js
@@ -1,28 +1,18 @@
 /*---------------------------------
   PC画面のサイドバーのアコーディオンメニュー
 ---------------------------------*/
-$(function() {
-  $('.side-bar-btn').click(function()  {
-    var category = $(this).hasClass('category-btn');
-    if(category){
-      $(this).nextAll('.panel').slideToggle();
+var btn = document.getElementsByClassName("btn");
+var i;
+for(i = 0; i < btn.length; i++) {
+  btn[i].addEventListener("click",function(){
+    var panel = this.nextElementSibling;
+    if(panel.style.display == "block") {
+      $(panel).slideUp(300);
+    }else {
+      $(panel).slideDown(300);
     }
-
-    
-  });
-});
-// var btn = document.getElementsByClassName("btn");
-// var i;
-// for(i = 0; i < btn.length; i++) {
-//   btn[i].addEventListener("click",function(){
-//     var panel = this.nextElementSibling;
-//     if(panel.style.display == "block") {
-//       $(panel).slideUp(300);
-//     }else {
-//       $(panel).slideDown(300);
-//     }
-//   })
-// }
+  })
+}
 
 $(function(){
 	$('.toggle_title').click(function(){
